7cab6886cc150b3fb90086022d97ce271fe72af8
shenxin <1683137709@qq.com>
2020-05-10 22:12:21 +0800

    修改类名隐藏实现

--- mallocator.hpp
+++ mallocator.hpp
@@ -7,44 +7,44 @@
 
 
 namespace sx {
-	template<typename T>
-	class allocator {
+template<typename T>
+class allocator {
 #ifndef USE_MALLOC_TEMPLATE
-		using alloc_template = default_alloc_template<false>;
+	using alloc_template = __default_alloc_template<false>;
 #else
-		using alloc_template = malloc_alloc_template<0>;
+	using alloc_template = malloc_alloc_template<0>;
 #endif // !USE_MALLOC_TEMPLATE
-	public:
-		static T *allocate() {
-			return reinterpret_cast<T *>(alloc_template::allocate(sizeof(T)));
-		}
+public:
+	static T *allocate() {
+		return reinterpret_cast<T *>(alloc_template::allocate(sizeof(T)));
+	}
 
-		static T *allocate(std::size_t n) {
-			return reinterpret_cast<T *>(alloc_template::allocate(sizeof(T) * n));
-		}
+	static T *allocate(std::size_t n) {
+		return reinterpret_cast<T *>(alloc_template::allocate(sizeof(T) * n));
+	}
 
-		static void deallocate(T *ptr) {
-			return alloc_template::deallocate(static_cast<void *>(ptr), sizeof(T));
-		}
+	static void deallocate(T *ptr) {
+		return alloc_template::deallocate(static_cast<void *>(ptr), sizeof(T));
+	}
 
-		static void deallocate(T *ptr, std::size_t n) {
-			return alloc_template::deallocate(static_cast<void *>(ptr), n);
-		}
+	static void deallocate(T *ptr, std::size_t n) {
+		return alloc_template::deallocate(static_cast<void *>(ptr), n);
+	}
 
-		template<typename... Args>
-		static void construct(T *ptr, Args&&... args) {
-			sx::construct(ptr, std::forward<Args>(args)...);
-		}
+	template<typename... Args>
+	static void construct(T *ptr, Args&&... args) {
+		sx::construct(ptr, std::forward<Args>(args)...);
+	}
 
-		static void destroy(T *ptr) {
-			sx::destroy(ptr);
-		}
+	static void destroy(T *ptr) {
+		sx::destroy(ptr);
+	}
 
-		template<typename ForwardIter>
-		static void destroy(ForwardIter first, ForwardIter last) {
-			sx::destroy(first, last);
-		}
-	};
+	template<typename ForwardIter>
+	static void destroy(ForwardIter first, ForwardIter last) {
+		sx::destroy(first, last);
+	}
+};
 }
 
 #endif
