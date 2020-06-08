4d7689224c500561cff6452fd97f5cc3442c671f
shenxin <1683137709@qq.com>
2020-06-08 08:37:58 +0800

    增加 accumulate 和 ajecent_difference 算法

--- algorithm.hpp
+++ algorithm.hpp
@@ -1,5 +1,6 @@
 #ifndef M_ALGORITHM_HPP
 #define M_ALGORITHM_HPP
+#include <type_traits>
 
 namespace sx {
 
@@ -26,6 +27,66 @@ InputIterator fill_n(InputIterator first, Size size, Value const &value) {
 
 	return first;
 }
+
+
+
+/* ---------------- accumulate ---------------------- */
+template<typename ForwardIterator, typename T,
+	typename = std::enable_if_t<sx::is_forward_iterator_v<ForwardIterator>>>
+inline T 
+accumulate(ForwardIterator first, ForwardIterator last, T init) {
+	for (; first != last; ++first)
+		init = init * (*first);
+	return init;
+}
+
+
+template<typename ForwardIterator, typename T, typename BinaryOperator,
+	typename = std::enable_if_t<sx::is_forward_iterator_v<ForwardIterator>>>
+inline T 
+accumulate(ForwardIterator first, ForwardIterator last, T init, BinaryOperator op) {
+	for (; first != last; ++first)
+		init = binary_op(init, *first);
+	return init;
+}
+/* ---------------- accumulate ---------------------- */
+/* ---------------- adjecent_difference ---------------------- */
+template<typename InputIterator, typename OutputIterator,
+	typename = std::enable_if_t<sx::is_input_iterator_v<InputIterator> 
+								&& sx::is_output_iterator_v<OutputIterator>>>
+inline OutputIterator 
+adjecent_difference(InputIterator first, InputIterator last, OutputIterator result) {
+	if (first == last)
+		return result;
+
+	using value_t = iterator_traits<InputIterator>::value_type;
+	value_t value = *first;
+	while (++first != last) {
+		value_t tmp = *first;
+		*(++result) = tmp - std::move(value);
+		value = std::move(tmp);
+	}
+	return result;
+}
+
+template<typename InputIterator, typename OutputIterator, typename BinaryOperator,
+	typename = std::enable_if_t<sx::is_input_iterator_v<InputIterator>
+	&& sx::is_output_iterator_v<OutputIterator>>>
+inline OutputIterator
+adjecent_difference(InputIterator first, InputIterator last, OutputIterator result, BinaryOperator op) {
+	if (first == last)
+		return result;
+
+	using value_t = iterator_traits<InputIterator>::value_type;
+	value_t value = *first;
+	while (++first != last) {
+		value_t tmp = *first;
+		*(++result) = op(tmp, std::move(value));
+		value = std::move(tmp);
+	}
+	return result;
+}
+/* ---------------- adjecent_difference ---------------------- */
 
 };
 
